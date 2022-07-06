6845dda0ba68cf9d8037021248b1b1b586d7b7b9
Nat Epstein <nathaniel.epstein@gmail.com>
2022-07-06 14:41:58 -0400

    added jquery and javascript files, randomized cpu pattern, added some functionality to userclicked pattern

---
+++ game.js
@@ -0,0 +1,21 @@
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
+var gamePattern = [];
+userClickedPattern = [];
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+})
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+}
+
+
